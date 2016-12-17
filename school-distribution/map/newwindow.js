6646c8af597c59848311573bd253a343ffcff2f9
timorleste <Think@Tonys-MacBook-Pro.local>
2016-12-17 21:37:42 +0900

    find
    find

---
+++ school-distribution/map/newwindow.js
@@ -0,0 +1,9 @@
+<!-- 
+  function PopUpNewWindow(targetURL)
+  {
+     NewWindow =
+		window.open(targetURL,'',
+'width=300,height=450,locationbar=yes,menubar=no,scrollbars=yes,status=no,resizable=yes,screenX=600,screenY=150');
+		NewWindow.focus();
+  }
+//-->
